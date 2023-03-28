48edc267ac5706556e5c9e4d0bf3844734600f5f
Infonite2413 <98448784+Infonite2413@users.noreply.github.com>
2023-03-28 15:16:59 +0800

    Data, not done yet

---
+++ sprint2dbPaintingsData.sql
@@ -0,0 +1,143 @@
+INSERT INTO `paintingstable` (`idPaintings`, `title`, `finished`, `media`, `artistFK`, `style`) VALUES
+
+(1,
+'Bal du moulin de la Galette',
+1876,
+'oil',
+'August Renoir',
+),
+
+(2,
+'Doni Tondo (Doni Madonna)',
+1507,
+'oil',
+'Michelangelo',
+'Mannerism',
+
+),
+
+(3,
+'Vase with Twelve Sunflowers ',
+1888,
+'oil',
+'Vincent Van Gogh',
+'Still-life',
+
+),
+
+(4,
+'Mona Lisa',
+1503,
+'oil',
+'Leonardo da Vinci',
+'Portrait',
+
+),
+
+(5,
+'The Potato Eaters',
+1885,
+'oil',
+'Vincent Van Gogh',
+'Realism',
+
+),
+
+(6,
+'Sunrise',
+1972,
+'oil',
+'Claude Monet',
+'Impressionism',
+
+),
+
+(7,
+'Weaver',
+1884,
+'oil',
+'Vaincent Van Gogh',
+'Realism',
+
+),
+
+(8,
+'Nature morte au compotier',
+1914,
+'oil',
+'Pablo Picasso',
+'Cubism',
+
+),
+
+(9,
+'Houses Of Parliament',
+1889,
+'oil',
+'Claude Monet',
+'Impressionism',
+
+),
+
+(10,
+'Cafe Terrace at Night',
+1888,
+'oil',
+'Vincent Van Gogh',
+'Impressionism',
+
+),
+
+(11,
+'At the Lapin Agile',
+1905,
+'oil',
+'Pablo Picasso',
+'Impressionism',
+
+),
+
+(12,
+'The Persistence of Memory',
+1931,
+'oil',
+'Salvador Dali',
+'Surrealism',
+
+),
+
+(13,
+'The Hallucinogenic Toreador',
+1970,
+'oil',
+'Salvador Dali',
+'Surrealism',
+
+),
+
+(14,
+'Jaz de Bouffan',
+1877,
+'oil',
+'Paul Cezanne',
+'Impressionism',
+
+),
+
+(15,
+'Vitruvian Man',
+1490,
+'pen-ink',
+'Leanardo da Vinci',
+'Realism',
+
+),
+
+(16,
+'The Kingfisher',
+1886,
+'pen-ink',
+'Vincent Van Gogh',
+'Realism',
+
+);

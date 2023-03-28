60c881ca2bda059e070931795f8ce10edb869950
Infonite2413 <98448784+Infonite2413@users.noreply.github.com>
2023-03-28 15:09:07 +0800

    Artist Data

---
+++ sprint2dbArtistData.sql
@@ -0,0 +1,78 @@
+INSERT INTO `artisttable` (`idArtist`, `artistName`, `artistBirth`, `artistDeath`, `artistNationality`) VALUES
+
+(1,
+'August Renoir',
+1841,
+1919,
+'French'
+),
+
+(2,
+'Michelangelo',
+1475,
+1564,
+'Italian'
+),
+
+(3,
+'Vincent Van Gogh',
+1853,
+1890,
+'Dutch'
+),
+
+(4,
+'Claude Monet',
+1840,
+1926,
+'French'
+),
+
+(5,
+'Rembrandt',
+1606,
+1699,
+'Dutch'
+),
+
+(6,
+'Pablo Picasso',
+1881,
+1973,
+'Spanish'
+),
+
+(7,
+'Jan Vermeer',
+1632,
+1675,
+'Dutch'
+),
+
+(8,
+'Salvador Dali',
+1904,
+1989,
+'Spanish'
+),
+
+(9,
+'Paul Cezanne',
+1839,
+1906,
+'French'
+),
+
+(10,
+'Leanardo da Vinci',
+1452,
+1519,
+'Italian'
+),
+
+(11,
+'Raphael',
+1483,
+1520,
+'Italian'
+);
